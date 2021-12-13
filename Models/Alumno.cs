1078a98bfb596c614efab36383df1a3e4c613a0e
joaquinoldani <joaquinoldani07@gmail.com>
2021-12-13 09:36:38 -0300

    modificacion de Models en base al diagrama de modelo de datos y carga de datos manual en caso de tener una BD vacia

--- Models/Alumno.cs
+++ Models/Alumno.cs
@@ -5,6 +5,9 @@ namespace platzi_asp_net_core.Models
 {
     public class Alumno: ObjetoEscuelaBase
     {
-        public List<Evaluacion> Evaluaciones { get; set; } = new List<Evaluacion>();
+        public List<Evaluacion> Evaluaciones { get; set; }
+
+        public string CursoId { get; set; }
+        public Curso Curso { get; set; }
     }
 }
--- Models/Asignatura.cs
+++ Models/Asignatura.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 namespace platzi_asp_net_core.Models
 {
     public class Asignatura:ObjetoEscuelaBase
     {
+        // referencio a la clase padre
+        public string CursoId { get; set; }
+        public Curso Curso { get; set; }
 
+        public List<Evaluacion> Evaluaciones { get; set; }
+        
     }
 }
