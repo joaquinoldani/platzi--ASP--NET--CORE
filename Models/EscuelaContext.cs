abb0dfbaae00f0649618b7dbda6cf8dae79d98e1
joaquinoldani <joaquinoldani07@gmail.com>
2021-12-14 09:09:08 -0300

    agregado de Data Annotations

--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -66,11 +66,11 @@ namespace platzi_asp_net_core.Models
         private static List<Curso> CargarCursos(Escuela escuela)
         {
             return new List<Curso>(){
-                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "101",Jornada = TiposJornada.Mañana},
-                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "201",Jornada = TiposJornada.Mañana},
-                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "301",Jornada = TiposJornada.Mañana},
-                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "401",Jornada = TiposJornada.Tarde},
-                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "501",Jornada = TiposJornada.Tarde}
+                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "101",Jornada = TiposJornada.Mañana, Dirección = "Av. Siempre Viva"},
+                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "201",Jornada = TiposJornada.Mañana, Dirección = "Av. Siempre Viva"},
+                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "301",Jornada = TiposJornada.Mañana, Dirección = "Av. Siempre Viva"},
+                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "401",Jornada = TiposJornada.Tarde, Dirección = "Av. Siempre Viva"},
+                new Curso(){Id = Guid.NewGuid().ToString(),EscuelaId = escuela.Id,Nombre = "501",Jornada = TiposJornada.Tarde, Dirección = "Av. Siempre Viva"}
             };
         }
     
